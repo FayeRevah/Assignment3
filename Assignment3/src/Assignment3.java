1124784b10e510ca3c56ab322d02520b21f1a253
rburkhardt-csumb <rburkhardt@csumb.edu>
2017-01-18 07:55:06 -0800

    Added the three classes

--- Assignment3/src/Assignment3.java
+++ Assignment3/src/Assignment3.java
@@ -1,26 +1,24 @@
 
-public class Assignment3
-{
+public class Assignment3 {
 
-   public static void main(String[] args)
-   {
-      // TODO Auto-generated method stub
+    public static void main(String[] args)
+    {
 
-   }
+    }
 
 }
 
 class Cards // Faye
 {
-    
+
 }
 
 class Hand // Oswaldo
 {
-    
+
 }
 
 class Deck // Roderick
 {
-    
+
 }
