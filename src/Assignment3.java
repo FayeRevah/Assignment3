5eed78680d467dec1b1868f95e95e20dff4f9c32
rburkhardt-csumb <rburkhardt@csumb.edu>
2017-01-18 08:01:55 -0800

    Add files via upload

--- src/Assignment3.java
+++ src/Assignment3.java
@@ -1,11 +1,48 @@
+/* ==================================================================
+*
+*   PROGRAM NAME:
+*       Assignment3 - Deck of Cards
+*
+*   Description:
+*	Here
+*
+*   Classes:
+*       Card, Hand, Deck
+*
+*   Parameters:
+*       1. none
+*
+*   Additional Files:
+*
+*   Created:
+*       2017/01/18
+*
+*   Author/s:
+*       Roderick Burkhardt, Oswaldo Minez, Faiga Revah
+*
+* ==================================================================*/
 
-public class Assignment3
+
+public class Assignment3 {
+
+    public static void main(String[] args)
+    {
+
+    }
+
+}
+
+class Cards // Faye
 {
 
-   public static void main(String[] args)
-   {
-      // TODO Auto-generated method stub
+}
 
-   }
+class Hand // Oswaldo
+{
+
+}
+
+class Deck // Roderick
+{
 
 }
