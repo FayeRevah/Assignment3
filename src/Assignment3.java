89660e30528044a51aec7fa79c98d5a0e1ba0749
rburkhardt-csumb <rburkhardt@csumb.edu>
2017-01-23 06:06:12 -0800

    Completed Deck Class & Test

--- src/Assignment3.java
+++ src/Assignment3.java
@@ -25,6 +25,59 @@
 import java.util.Random;
 
 
+public class Assignment3 {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Deck Test");
+        
+        Deck testDeck = new Deck(2);
+        int cardCount = testDeck.getTopCard() +1;
+        while (cardCount > 0)
+        {
+            System.out.print(testDeck.dealCard() + "  /  ");
+            cardCount--;
+        }
+        System.out.println("");
+        System.out.println("");
+        
+        testDeck.init(2);
+        testDeck.shuffle();
+        cardCount = testDeck.getTopCard() +1;
+        while (cardCount > 0)
+        {
+            System.out.print(testDeck.dealCard() + "  /  ");
+            cardCount--;
+        }
+        System.out.println("");
+        System.out.println("");
+        
+        testDeck.init(1);
+        cardCount = testDeck.getTopCard() +1;
+        while (cardCount > 0)
+        {
+            System.out.print(testDeck.dealCard() + "  /  ");
+            cardCount--;
+        }
+        System.out.println("");
+        System.out.println("");
+        
+        testDeck.init(1);
+        testDeck.shuffle();
+        cardCount = testDeck.getTopCard() +1;
+        while (cardCount > 0)
+        {
+            System.out.print(testDeck.dealCard() + "  /  ");
+            cardCount--;
+        }
+        System.out.println("");
+        System.out.println("");
+        
+    }
+
+}
+
+
 // Faye
 class Card
 {
@@ -144,20 +197,20 @@ class Deck //Roderick
 
     public Deck()
     {
-        allocateMasterPack();
-        this.numPacks = 1;
-        init(this.numPacks);
+        init(1);
     }
 
     public Deck(int numPacks)
     {
-        allocateMasterPack();
-        this.numPacks = numPacks;
-        init(this.numPacks);
+        init(numPacks);
     }
 
     public void init(int numPacks)
     {
+        allocateMasterPack();
+        this.numPacks = numPacks;
+        this.topCard = (52 * this.numPacks) - 1;
+        
         cards = new Card[52 * numPacks];
 
         for (int pack = 0; pack < numPacks; pack++)
@@ -173,25 +226,32 @@ class Deck //Roderick
     {
         int split = cards.length / 2;
         Random rand = new Random();
+        int shufCount = 5 * (52 * numPacks);
 
-        for (int shufTurn = 0; shufTurn < 5; shufTurn++)
-        {
-            for (int card = 0; card <= split;)
-            {
-                int shuffleCard = rand.nextInt(3);
-                Card temp = cards[card + shuffleCard];
-                cards[card + shuffleCard] = cards[card];
-                cards[card] = temp;
-
-                card += shuffleCard;
-            }
-        }
+        do
+        {            
+            int cardA = rand.nextInt(split);
+            int cardB = rand.nextInt(split) + split;
+            
+            Card temp = cards[cardA];
+            cards[cardA] = cards[cardB];
+            cards[cardB] = temp;
+            
+            shufCount--;
+            
+        } while (shufCount != 0);
 
     }
 
     public Card dealCard()
     {
-        return new Card();
+        Card retVal = cards[topCard];
+        cards[topCard] = null;
+        //System.out.println(topCard);
+        if (topCard > 0)
+            topCard--;
+        //System.out.println(topCard);
+        return retVal;
     }
 
     public int getTopCard()
@@ -199,9 +259,9 @@ class Deck //Roderick
         return topCard;
     }
 
-    public Card inspectCar(int k)
+    public Card inspectCard(int k)
     {
-        return new Card();
+        return cards[k];
     }
 
     private static void allocateMasterPack()
@@ -211,18 +271,42 @@ class Deck //Roderick
             masterPack = new Card[52];
             
             for (Card.Suit s : Card.Suit.values())
-                
-            /*for (int x = 0; x < masterPack.length; x++)
             {
-                int enumChange = 0;
-                if (x % 26 == 0)
+                for (int x = 1; x < 14; x++)
                 {
-                    enumChange++;
+                    Card newCard;
+                    int value = x % 14;
+                    switch (value)
+                    {
+                        case 1:
+                            newCard = new Card('A', s);
+                            break;
+                        case 10:
+                            newCard = new Card('T', s);
+                            break;
+                        case 11:
+                            newCard = new Card('J', s);
+                            break;
+                        case 12:
+                            newCard = new Card('Q', s);
+                            break;
+                        case 13:
+                            newCard = new Card('K', s);
+                            break;
+                        default:
+                            newCard = new Card(Integer.toString(value).charAt(0), s);
+                    }
+                    if (s == Card.Suit.clubs) masterPack[x-1] = newCard;
+                    else if (s == Card.Suit.diamonds) masterPack[(x-1)+13] =
+                            newCard;
+                    else if (s == Card.Suit.hearts) masterPack[(x-1)+26] =
+                            newCard;
+                    else masterPack[(x-1)+39] = newCard;                    
                 }
-
-                masterPack[x] = new Card();
-            }*/
-            System.out.println("master pack created");
+            } 
+            //for (Card x : masterPack)
+            //    System.out.println(x);
+            //System.out.println("master pack created");
         }
     }
 }
