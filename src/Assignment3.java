15fdc31e8f41651f6e13b69ef00e19aa34e22dd8
rburkhardt-csumb <rburkhardt@csumb.edu>
2017-01-23 21:45:25 -0800

    update main()

--- src/Assignment3.java
+++ src/Assignment3.java
@@ -22,14 +22,16 @@
 *
 * ==================================================================*/
 
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
-
-public class Assignment3 {
+public class Assignment3
+{
 
     public static void main(String[] args)
     {
-        System.out.println("Deck Test");
+        /*System.out.println("Deck Test");
         
         Deck testDeck = new Deck(2);
         int cardCount = testDeck.getTopCard() +1;
@@ -71,12 +73,82 @@ public class Assignment3 {
             cardCount--;
         }
         System.out.println("");
+        System.out.println("");*/
+
+        Deck playDeck = new Deck(4);
+        int deckSize = playDeck.getTopCard();
+        int numPlayers;
+        ArrayList<Hand> playersHands;
+        boolean numPlayerGood = false;
+        Scanner readIn = new Scanner(System.in);
+
+        do
+        {
+            System.out.print("How many players? (1 - 10, please): ");
+            numPlayers = readIn.nextInt();
+            if (numPlayers > 0 && numPlayers < 11)
+            {
+                numPlayerGood = true;
+            }
+            if (!numPlayerGood)
+            {
+                System.out.println("Invalid input, please try again!");
+            }
+        } while (!numPlayerGood);
+
+        playersHands = new ArrayList<>();
+        for ( int p = 0; p < numPlayers; p++)
+            playersHands.add(new Hand());
+
+        for ( int dc = 6 ; dc > 0 ; dc-- )
+            for ( Hand playerHand : playersHands )
+                playerHand.takeCard(playDeck.dealCard());
+
+        System.out.println("Here is the players hands from an unshuffled deck: ");
+        for ( Hand playerHand : playersHands )
+        {
+            System.out.println(playerHand);
+            System.out.println("");
+        }
+        
+        System.out.println("");
         System.out.println("");
         
+        playDeck.init(4);
+        playDeck.shuffle();
+        
+        for ( Hand playerHand : playersHands )
+            playerHand.resetHand();
+
+        for ( int dc = 6 ; dc > 0 ; dc-- )
+            for ( Hand playerHand : playersHands )
+                playerHand.takeCard(playDeck.dealCard());
+
+        System.out.println("Here is the players hands from a shuffled deck: ");
+        for ( Hand playerHand : playersHands )
+        {
+            System.out.println(playerHand);
+            System.out.println("");
+        }
+        
+        /*Card test1 = new Card('A', Card.Suit.clubs);
+        Card test2 = new Card('8', Card.Suit.hearts);
+        Card test3 = new Card('K', Card.Suit.spades);
+        Card test4 = new Card('J', Card.Suit.diamonds);
+        Card test5 = new Card('4', Card.Suit.spades);
+        
+        Hand testHand = new Hand();
+        testHand.takeCard(test1);
+        testHand.takeCard(test2);
+        testHand.takeCard(test3);
+        testHand.takeCard(test4);
+        testHand.takeCard(test5);
+        
+        System.out.println(testHand);*/
+
     }
 
 }
-
 
 // Faye
 class Card
@@ -182,6 +254,86 @@ class Card
 class Hand //Oswaldo
 {
 
+    public static int MAX_CARDS = 50;
+    private Card[] myCards;
+    private int numCards;
+
+    // Default Constructor
+    public Hand()
+    {
+        myCards = new Card[MAX_CARDS];
+        numCards = 0;
+    }
+
+    // Method that removes all cards from the Hand[] array
+    public void resetHand()
+    {
+        myCards = new Card[MAX_CARDS];
+        numCards = 0;
+    }
+
+    // This method adds cards to the next available position
+    public boolean takeCard(Card card)
+    {
+        if (numCards >= MAX_CARDS)
+        {
+            return false;
+        }
+        else
+        {
+            myCards[numCards++] = card;
+            //numCards++;
+            return true;
+        }
+    }
+
+    // This method returns and removes the card in the top position of the array
+    public Card playCard()
+    {
+        Card card = myCards[numCards - 1];
+        myCards[numCards - 1] = null;
+        return card;
+    }
+
+    // This method is a stringizer that is used prior to displaying the entire hand
+    public String toString()
+    {
+        String str = "Hand = ( ";
+        if (numCards == 0)
+        {
+            str += "empty hand )";
+        }
+        else
+        {
+            for (int i = 0; i < numCards - 1; i++)
+            {
+                str += myCards[i] + ", ";
+            }
+            str += myCards[numCards - 1] + " )";
+        }
+        return str;
+    }
+
+    // Getter for numCards
+    public int getNumCards()
+    {
+        return numCards;
+    }
+
+    // Accessor to inspect an individual card, returns erroFlag = true if k is bad
+    public Card inspectCard(int k)
+    {
+        Card card;
+        if (k <= numCards)
+        {
+            card = new Card('T', Card.Suit.spades);
+        }
+        else
+        {
+            card = myCards[k - 1];
+        }
+        return card;
+    }
 }
 
 class Deck //Roderick
@@ -210,7 +362,7 @@ class Deck //Roderick
         allocateMasterPack();
         this.numPacks = numPacks;
         this.topCard = (52 * this.numPacks) - 1;
-        
+
         cards = new Card[52 * numPacks];
 
         for (int pack = 0; pack < numPacks; pack++)
@@ -229,16 +381,15 @@ class Deck //Roderick
         int shufCount = 5 * (52 * numPacks);
 
         do
-        {            
+        {
             int cardA = rand.nextInt(split);
             int cardB = rand.nextInt(split) + split;
-            
+
             Card temp = cards[cardA];
             cards[cardA] = cards[cardB];
             cards[cardB] = temp;
-            
+
             shufCount--;
-            
         } while (shufCount != 0);
 
     }
@@ -247,10 +398,10 @@ class Deck //Roderick
     {
         Card retVal = cards[topCard];
         cards[topCard] = null;
-        //System.out.println(topCard);
         if (topCard > 0)
+        {
             topCard--;
-        //System.out.println(topCard);
+        }
         return retVal;
     }
 
@@ -269,7 +420,7 @@ class Deck //Roderick
         if (masterPack == null)
         {
             masterPack = new Card[52];
-            
+
             for (Card.Suit s : Card.Suit.values())
             {
                 for (int x = 1; x < 14; x++)
@@ -296,17 +447,26 @@ class Deck //Roderick
                         default:
                             newCard = new Card(Integer.toString(value).charAt(0), s);
                     }
-                    if (s == Card.Suit.clubs) masterPack[x-1] = newCard;
-                    else if (s == Card.Suit.diamonds) masterPack[(x-1)+13] =
-                            newCard;
-                    else if (s == Card.Suit.hearts) masterPack[(x-1)+26] =
-                            newCard;
-                    else masterPack[(x-1)+39] = newCard;                    
+                    if (s == Card.Suit.clubs)
+                    {
+                        masterPack[x - 1] = newCard;
+                    }
+                    else if (s == Card.Suit.diamonds)
+                    {
+                        masterPack[(x - 1) + 13]
+                                = newCard;
+                    }
+                    else if (s == Card.Suit.hearts)
+                    {
+                        masterPack[(x - 1) + 26]
+                                = newCard;
+                    }
+                    else
+                    {
+                        masterPack[(x - 1) + 39] = newCard;
+                    }
                 }
-            } 
-            //for (Card x : masterPack)
-            //    System.out.println(x);
-            //System.out.println("master pack created");
+            }
         }
     }
 }
